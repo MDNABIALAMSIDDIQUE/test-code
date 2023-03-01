f6bf93e7a4c8cb226d64eca64d0c8de2bf76b42a
MDNABIALAMSIDDIQUE <nabijavamd@gmail.com>
2023-03-01 18:20:00 +0530

    ConfigurationPropaties

---
+++ SpringBoot_05_ConfigurationPropaties/src/test/java/com/gtl/SpringBoot05ConfigurationPropatiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gtl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot05ConfigurationPropatiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
